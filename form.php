6d8df91e5a5fadba71bf1266d85662cedaa5887b
chicadeamarilla <rajabisheyda15@gmail.com>
2025-07-27 22:44:21 +0330

    form.php

---
+++ form.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html>
+<body>
+    <h2>Enter your name</h2>
+    <form action="set_cookie.php" method="post">
+        <label for="username">Name:</label>
+        <input type="text" id="username" name="username" required>
+        <input type="submit" value="Submit">
+    </form>
+</body>
+</html>
